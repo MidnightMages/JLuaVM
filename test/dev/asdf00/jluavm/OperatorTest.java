6b2c219e5a7b96bfb8d20aa772c4538c5f58e9cb
GHXX <GHXX@gmx.at>
2024-09-25 23:42:15 +0200

    add a few tests

---
+++ test/dev/asdf00/jluavm/OperatorTest.java
@@ -0,0 +1,10 @@
+package dev.asdf00.jluavm;
+
+import org.junit.jupiter.api.Test;
+
+class OperatorTest {
+    @Test
+    void floorDiv() {
+        // TODO assert that a//b == floor((float)a/(float)b) for pos and negative
+    }
+}
