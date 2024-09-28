f1cc4d6bedd70c30acac0ea4cc2f05feeb8e4f1f
GHXX <GHXX@gmx.at>
2024-09-28 02:51:42 +0200

    add luavm api

--- src/dev/asdf00/jluavm/LuaVM.java
+++ src/dev/asdf00/jluavm/LuaVM.java
@@ -1,5 +1,19 @@
 package dev.asdf00.jluavm;
 
 public class LuaVM {
+    public void load(String code) {
 
+    }
+
+    public VmResult run() {
+        return new VmResult(VmRunState.SUCCESS, new Object[]{});
+    }
+
+    public record VmResult(VmRunState state, Object[] returnVars) {
+    }
+
+    public enum VmRunState {
+        SUCCESS,
+        EXECUTION_ERROR,
+    }
 }
