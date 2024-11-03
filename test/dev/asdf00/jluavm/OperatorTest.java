0c5b51dd2e4048293044cf0d0d2f400a17cb12c6
GHXX <GHXX@gmx.at>
2024-11-04 00:24:29 +0100

    add a few rt operators, update todo

--- test/dev/asdf00/jluavm/OperatorTest.java
+++ test/dev/asdf00/jluavm/OperatorTest.java
@@ -7,4 +7,6 @@ class OperatorTest {
     void floorDiv() {
         // TODO assert that a//b == floor((float)a/(float)b) for pos and negative
     }
+
+    // TODO add mod test covering positive and negatives
 }
