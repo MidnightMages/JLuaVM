0ae0f4120b87cb7fa48cbcda58b0772aed02c99e
GHXX <GHXX@gmx.at>
2024-11-04 00:32:31 +0100

    update todo

--- test/dev/asdf00/jluavm/OperatorTest.java
+++ test/dev/asdf00/jluavm/OperatorTest.java
@@ -9,4 +9,7 @@ class OperatorTest {
     }
 
     // TODO add mod test covering positive and negatives
+
+    // TODO add test for ensuring (-1 >> 60) === 15
+    // TODO add test for ensuring (-1.0 >> 60) === 15
 }
