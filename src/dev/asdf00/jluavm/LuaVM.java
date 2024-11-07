ca3c7b6de0eda0f12db7c975dcbba9e64b8142a8
GHXX <GHXX@gmx.at>
2024-11-07 16:16:04 +0100

    prototype std lib injection

--- src/dev/asdf00/jluavm/LuaVM.java
+++ src/dev/asdf00/jluavm/LuaVM.java
@@ -1,11 +1,28 @@
 package dev.asdf00.jluavm;
 
 import dev.asdf00.jluavm.internals.LuaVM_RT;
+import dev.asdf00.jluavm.runtime.types.AtomicLuaFunction;
+import dev.asdf00.jluavm.runtime.types.LuaObject;
 
 public abstract class LuaVM {
 
     public static LuaVM create() {
         return new LuaVM_RT();
+    }
+
+    public LuaVM withStdLib() {
+        var _G = LuaObject.table();
+
+        // https://www.lua.org/manual/5.4/manual.html#6.7
+        var mathTbl = LuaObject.table();
+        _G.set("math", mathTbl);
+        mathTbl.set("abs", AtomicLuaFunction.forOneResult((vm, x) -> {
+            if (x.isNumber()) return x.asDouble() < 0 ? x.unm() : x;
+            vm.errorArgType(1, "number", x);
+            return LuaObject.NIL;
+        }).obj());
+
+        return this;
     }
 
     public void load(String code) {
