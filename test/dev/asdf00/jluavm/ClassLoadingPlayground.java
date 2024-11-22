db8a0e104c41baeeb9613753ab6230d700440965
00asdf <christoph@00asdf.dev>
2024-11-22 23:47:44 +0100

    autoformat ClassLoadingPlayground

--- test/dev/asdf00/jluavm/ClassLoadingPlayground.java
+++ test/dev/asdf00/jluavm/ClassLoadingPlayground.java
@@ -9,35 +9,35 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ClassLoadingPlayground {
     private static final String VERY_SIMPLE_CLASS = """
             package dev.asdf00.jluavm.lualoaded;
-                import dev.asdf00.jluavm.exceptions.InternalLuaRuntimeError;
-                import dev.asdf00.jluavm.exceptions.LuaRuntimeError;
-                import dev.asdf00.jluavm.internals.LuaVM_RT;
-                import dev.asdf00.jluavm.runtime.errors.*;
-                import dev.asdf00.jluavm.runtime.types.*;
-                import dev.asdf00.jluavm.runtime.utils.*;
-                import java.lang.reflect.Constructor;
-                public final class %s extends LuaFunction {
-                    public static Constructor<? extends LuaFunction>[] innerFunctions;
-                    public %s(LuaObject[] _ENV, LuaObject[] closures) {
-                       super(_ENV, closures);
-                    }
-                    @Override
-                    public int getMaxLocalsSize() {
-                        return 0;
-                    }
-                    @Override
-                    public int getArgCount() {
-                        return 0;
-                    }
-                    @Override
-                    public boolean hasParamsArg() {
-                        return false;
-                    }
-                    @Override
-                    public void invoke(LuaVM_RT vm, LuaObject[] stackFrame, int resume, LuaObject[] expressionStack, LuaObject[] returned) {
-                        System.out.println("called %s#invoke");
-                    }
-                }""";
+            import dev.asdf00.jluavm.exceptions.InternalLuaRuntimeError;
+            import dev.asdf00.jluavm.exceptions.LuaRuntimeError;
+            import dev.asdf00.jluavm.internals.LuaVM_RT;
+            import dev.asdf00.jluavm.runtime.errors.*;
+            import dev.asdf00.jluavm.runtime.types.*;
+            import dev.asdf00.jluavm.runtime.utils.*;
+            import java.lang.reflect.Constructor;
+            public final class %s extends LuaFunction {
+                public static Constructor<? extends LuaFunction>[] innerFunctions;
+                public %s(LuaObject[] _ENV, LuaObject[] closures) {
+                   super(_ENV, closures);
+                }
+                @Override
+                public int getMaxLocalsSize() {
+                    return 0;
+                }
+                @Override
+                public int getArgCount() {
+                    return 0;
+                }
+                @Override
+                public boolean hasParamsArg() {
+                    return false;
+                }
+                @Override
+                public void invoke(LuaVM_RT vm, LuaObject[] stackFrame, int resume, LuaObject[] expressionStack, LuaObject[] returned) {
+                    System.out.println("called %s#invoke");
+                }
+            }""";
 
     private static Class<?> compileSimpleClass(String className) throws ReflectException {
         return Reflect.compile("dev.asdf00.jluavm.lualoaded." + className, VERY_SIMPLE_CLASS.formatted(className, className, className)).type();
