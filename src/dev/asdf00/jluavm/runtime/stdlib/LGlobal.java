e8e06561152f28bd240f30057cac951b9f418e1d
GHXX <GHXX@gmx.at>
2025-09-10 06:36:33 +0200

    fix default chunk name

--- src/dev/asdf00/jluavm/runtime/stdlib/LGlobal.java
+++ src/dev/asdf00/jluavm/runtime/stdlib/LGlobal.java
@@ -337,7 +337,7 @@ public class LGlobal {
 
 
                     try {
-                        var rv2 = LuaVM.load(chunkName == null ? "nil" : chunkName.asString(), chunk.getString(), env == null ? vm.getCallerEnv() : env);
+                        var rv2 = LuaVM.load(chunkName == null ? "=(load)" : chunkName.asString(), chunk.getString(), env == null ? vm.getCallerEnv() : env);
                         return new LuaObject[]{LuaObject.of(rv2), LuaObject.NIL};
                     } catch (LuaParserException ex) {
                         return new LuaObject[]{LuaObject.NIL, LuaObject.of("Compilation error: " + ex.getMessage())};
