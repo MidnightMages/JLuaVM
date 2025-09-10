ee27ee86ad225e7342ec62caf36a746746250b32
00asdf <christoph@00asdf.dev>
2025-09-10 14:50:12 +0200

    drop erroneous test (replacement in TracebackTest)

--- test/dev/asdf00/jluavm/vm/VmTest.java
+++ test/dev/asdf00/jluavm/vm/VmTest.java
@@ -696,7 +696,7 @@ public class VmTest extends BaseVmTest {
         var vm = assertDoesNotThrow(() -> LuaVM.builder().rootFunc("""
                 local function test(t)
                     return "received " .. tostring(t)
-                end 
+                end
                 return pcall(test)
                 """)).build();
         var res = vm.run();
@@ -2654,38 +2654,6 @@ public class VmTest extends BaseVmTest {
                 logString([[]])
                 return rv
                 """, LuaObject.of(";5;5;4;0;0"));
-    }
-
-    @Test
-    void tracebackFormat() {
-        loadAssertSuccessAndRv("""
-                local tb
-                function a(i)
-                    function b(i)
-                        tb = debug.traceback()
-                    end
-                    if i > 0 then a(i-1) else b() end
-                end
-                                
-                a(10)
-                return tb
-                """, LuaObject.of("""
-                stack traceback:
-                	main.lua:4: in function 'b'
-                	main.lua:6: in function 'a'
-                	main.lua:6: in function 'a'
-                	main.lua:6: in function 'a'
-                	main.lua:6: in function 'a'
-                	main.lua:6: in function 'a'
-                	main.lua:6: in function 'a'
-                	main.lua:6: in function 'a'
-                	main.lua:6: in function 'a'
-                	main.lua:6: in function 'a'
-                	main.lua:6: in function 'a'
-                	main.lua:6: in function 'a'
-                	main.lua:9: in main chunk
-                	[C]: in ?
-                """));
     }
 
     @Test
